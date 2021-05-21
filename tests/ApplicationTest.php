7d187e4af8c294cfff60588e6a98d40a5f9bec70
jie-mini <>
2021-05-21 10:47:27 +0800

    fix

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -16,6 +16,7 @@ class ApplicationTest extends TestCase
             'app_id' => 'JFytU4V73',
             'app_secret' => 'APUUYTnvfw97H9v3',
         ];
+        
         $app = new Application($config);
         $this->assertTrue(true);
         return $app;
