51dddfd878069d375efefff3118b35a780453b65
jie-mini <>
2021-05-25 09:27:10 +0800

    保存入职离职时间

--- src/Organization/Service.php
+++ src/Organization/Service.php
@@ -8,20 +8,22 @@ use Wwjpackages\CloudFinance\Kernel\BaseService;
 
 class Service extends BaseService
 {
-	const SyncEmployee = '/api/api/organizations/employees/sync';
+    const SyncEmployee = '/api/api/organizations/employees/sync';
 
-	public function syncEmployee(array $data): array
-	{
-		$params = [
-			'real_name' => $data['real_name'],
-			'employee_no' => $data['employee_no'],
-			'organization_name' => $data['organization_name'],
-			'job_title' => $data['job_title'],
-			'id_card' => $data['id_card'],
-			'mobile' => $data['mobile'],
-			'status' => $data['status'],
-			'comments' => $data['comments'],
-		];
-		return $this->attempt('post', self::SyncEmployee, $params);
-	}
+    public function syncEmployee(array $data): array
+    {
+        $params = [
+            'real_name' => $data['real_name'],
+            'employee_no' => $data['employee_no'],
+            'organization_name' => $data['organization_name'],
+            'job_title' => $data['job_title'],
+            'id_card' => $data['id_card'],
+            'mobile' => $data['mobile'],
+            'status' => $data['status'],
+            'entry_date' => $data['entry_date'],
+            'dimission_date' => $data['dimission_date'] ?: '',
+            'comments' => $data['comments'] ?: '',
+        ];
+        return $this->attempt('post', self::SyncEmployee, $params);
+    }
 }
