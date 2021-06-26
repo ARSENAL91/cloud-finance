f8f555da83d605c5aac5303a851b0b20909a685d
dengruiqiang <1020229357@qq.com>
2021-06-26 16:47:54 +0800

    同步固定资产

---
+++ src/Property/Service.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Wwjpackages\CloudFinance\Subject;
+
+
+use Wwjpackages\CloudFinance\Kernel\BaseService;
+
+class Service extends BaseService
+{
+    const URL = '/api/api/property/sync';
+
+    public function index(array $data): array
+    {
+        $params = [
+            'buy_date' => $data['buy_date'] ?? date('Y-m-d H:i:s'),
+            'sync_type' => $data['is_last'] ?? 'n',
+            'porperty' => $data['porperty'] ?? [],
+            'purchase' => $data['purchase'] ?? [],
+        ];
+        return $this->attempt('post', self::URL, $params);
+    }
+}
---
+++ src/Property/ServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Wwjpackages\CloudFinance\Subject;
+
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+
+
+    public function register(Container $app)
+    {
+        $app['property'] = function ($app) {
+            return new Service($app->getConfig(), $app['httpClient']);
+        };
+    }
+
+}
