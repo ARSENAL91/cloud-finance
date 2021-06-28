1ef9a7ab86dc9d35f61e52d568228af3684ced6d
dengruiqiang <1020229357@qq.com>
2021-06-28 14:37:12 +0800

    同步固定资产

--- src/Property/Service.php
+++ src/Property/Service.php
@@ -10,12 +10,12 @@ class Service extends BaseService
 {
     const URL = '/api/api/property/sync';
 
-    public function index(array $data): array
+    public function sync(array $data): array
     {
         $params = [
-            'buy_date' => $data['buy_date'] ?? date('Y-m-d H:i:s'),
-            'sync_type' => $data['is_last'] ?? 'n',
-            'porperty' => $data['porperty'] ?? [],
+            'buy_date' => $data['buy_date'] ?? '',
+            'sync_type' => $data['sync_type'] ?? 'n',
+            'property' => $data['property'] ?? [],
             'purchase' => $data['purchase'] ?? [],
         ];
         return $this->attempt('post', self::URL, $params);
