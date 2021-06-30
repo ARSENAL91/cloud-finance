e75b7630681a00f1bd83d02355ffa1ad28f3cbda
jie-mini <>
2021-06-30 19:13:48 +0800

    同步工资

--- src/Wages/Service.php
+++ src/Wages/Service.php
@@ -8,13 +8,28 @@ use Wwjpackages\CloudFinance\Kernel\BaseService;
 
 class Service extends BaseService
 {
-    const SyncWages = '/api/api/organizations/wages/sync';
+    const SyncWages = '/api/api/wages/sync';
 
     public function syncWages(array $data): array
     {
-        $params = [
+        $params = [];
+        foreach ($data as $v){
+            $params['list'][] = [
+                'organization_name' => $v['organization_name'],
+                'employee_no' => $v['employee_no'],
+                'the_month' => $v['the_month'],
+                'base_wage' => $v['base_wage'],
+                'person_insurance' => $v['person_insurance'],
+                'company_insurance' => $v['company_insurance'],
+                'person_accumulation' => $v['person_accumulation'],
+                'company_accumulation' => $v['company_accumulation'],
+                'gross_special_tax' => $v['gross_special_tax'],
+                'person_tax' => $v['person_tax'],
+                'actual_wage' => $v['actual_wage'],
+                'agile_detail' => $v['agile_detail'],
+            ];
+        }
 
-        ];
         return $this->attempt('post', self::SyncWages, $params);
     }
 }
