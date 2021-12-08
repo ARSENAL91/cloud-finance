51373df95e9ff6ee297be92696bc088889101273
邓锐强 <dengruiqiang@ydjdev.com>
2021-12-08 18:22:56 +0800

    总部往来结算

--- src/ContactsSettlement/Service.php
+++ src/ContactsSettlement/Service.php
@@ -9,12 +9,22 @@ use Wwjpackages\CloudFinance\Kernel\BaseService;
 class Service extends BaseService
 {
     const URL = '/api/api/voucher/getSixExchangesData';
+    const URLMONTH = '/api/api/voucher/getSixExchangesMonthData';
     public function index(array $data): array
     {
         $params = [
             'key' => $data['key'] ?? '',
         ];
         return $this->attempt('post', self::URL, $params);
+    }
+    public function monthData(array $data): array
+    {
+        $params = [
+            'key' => $data['key'] ?? '',
+            'start_date' => $data['start_date'] ?$data['start_date']: '0000-00-00',
+            'end_date' => $data['end_date'] ? $data['end_date']: '0000-00-00',
+        ];
+        return $this->attempt('post', self::URLMONTH, $params);
     }
 
 }
