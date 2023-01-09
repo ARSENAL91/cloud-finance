4ee65d35c5df6066b2dcfd7f9e271e81e894a5aa
邬伟杰 <wuweijie@ydjdev.com>
2023-01-09 17:43:47 +0800

    获取当月固定资产折旧金额

--- src/Property/Service.php
+++ src/Property/Service.php
@@ -9,6 +9,7 @@ use Wwjpackages\CloudFinance\Kernel\BaseService;
 class Service extends BaseService
 {
     const URL = '/api/api/property/sync';
+    const DepreciationAmount = '/api/api/property/depreciation/amount/sum';
 
     public function sync(array $data): array
     {
@@ -20,4 +21,14 @@ class Service extends BaseService
         ];
         return $this->attempt('post', self::URL, $params);
     }
+
+    public function getSumDepreciationAmountToCw(array $data): array
+    {
+        $params = [
+            'the_month' => $data['the_month'] ,
+            'account_set_month' => $data['the_month'] ,
+        ];
+        return $this->attempt('get', self::DepreciationAmount, $params);
+    }
+
 }
