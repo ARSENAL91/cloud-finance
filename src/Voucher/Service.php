44902ee86562c47524c1a33fec74084e2303993c
古维 <guwei@ydjdev.com>
2023-05-16 09:08:24 +0800

    coding voucher

--- src/Voucher/Service.php
+++ src/Voucher/Service.php
@@ -9,17 +9,9 @@ use Wwjpackages\CloudFinance\Kernel\BaseService;
 class Service extends BaseService
 {
     const URL = '/api/api/voucher/checkHasVoucher';
+    const STORE = '/api/api/vouchers';
+    const INDEX = '/api/api/vouchers';
 
-    public function index(array $data): array
-    {
-        $params = [
-            'company_name' => $data['company_name'] ?? '',//公司名称
-            'receipt_type' => $data['receipt_type'] ?? [],//单据
-            'product_kind' => $data['product_kind'] ?? [],//类型
-            'the_month' => $data['the_month'] ?? [],//日期
-        ];
-        return $this->attempt('get', self::URL, $params);
-    }
 
     public function checkHasVoucher(array $data): array
     {
@@ -30,6 +22,28 @@ class Service extends BaseService
             'the_month' => $data['the_month'] ?? [],//日期
         ];
         return $this->attempt('get', self::URL, $params);
+    }
+
+    public function index(array $data): array
+    {
+        $params = [
+            'ids' => $data['ids']
+        ];
+        return $this->attempt('get', self::INDEX, $params);
+    }
+
+    public function store(array $data): array
+    {
+        $params = [
+            'company_id' => $data['company_id'] ?? 0,
+            'account_set_id' => $data['account_set_id'] ?? 0,
+            'ADR_id' => $data['ADR_id'] ?? 0,
+            'serial_num' => $data['serial_num'] ?? '',
+            'the_date' => $data['the_date'],
+            'source' => $data['source'],
+            'records' => $data['records']
+        ];
+        return $this->attempt('post', self::STORE, $params);
     }
 
 }
